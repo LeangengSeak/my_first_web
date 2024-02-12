cbf8d0c4b26277658ce67d28b3a4510bb413fd0b
LeangengSeak <leangeng03@gmail.com>
2024-02-12 13:46:41 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+    
+    const date = new Date();
+    const hour = date.getHours();
+    console.log(hour);
+
+    let message = "";
+    if(hour <= 12) message = "\"Good Morning\"";
+    if(hour > 12) message = "\"Good Afternoon\"";
+    if(hour >= 17) message = "\"Good Evening\"";
+
+    $('#headingtext').empty().append(message + " From Leangeng Seak");
+
+}) 
